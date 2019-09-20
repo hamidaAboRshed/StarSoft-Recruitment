25d2f2510e83119fc16098fd9ef4b1443161c117
Taghreed <taghreed.kraiker@gmail.com>
2019-09-20 12:39:00 +0300

    apply template

---
+++ application/views/Home.php
@@ -0,0 +1,240 @@
+<!-- Start header section -->  
+<header id="header">
+    <div class="header-inner">
+      <!-- Header image -->
+      <img src="<?php echo base_url();?>assets/images/header-bg.jpg" alt="img">
+      <div class="header-overlay">
+        <div class="header-content">
+        <!-- Start header content slider -->
+        <h2 class="header-slide">WE GENERATE
+          <span>MOST CREATIVE</span>
+          <span>MOST Design</span>
+          <span>MOST Valuable</span>
+          IDEA</h2>
+        <!-- End header content slider -->  
+        <!-- Header btn area -->
+        <div class="header-btn-area">
+          <a class="knowmore-btn" href="#">KNOW MORE</a>
+          <a class="download-btn" href="#">DOWNLOAD Theme</a>
+        </div>
+      </div>
+      </div>      
+    </div>
+  </header>
+  <!-- End header section -->
+
+  <!-- Start menu section -->
+  <section id="menu-area">
+    <nav class="navbar navbar-default main-navbar" role="navigation">  
+      <div class="container">
+        <div class="navbar-header">
+          <!-- FOR MOBILE VIEW COLLAPSED BUTTON -->
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- LOGO -->                                               
+           <a class="navbar-brand logo" href="index.html"><img src="<?php echo base_url();?>assets/images/logo.jpg" alt="logo"></a>                      
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul id="top-menu" class="nav navbar-nav main-nav menu-scroll">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="#about">ABOUT</a></li> 
+            <li><a href="#team">TEAM</a></li>                    
+            <li><a href="#service">SERVICE</a></li> 
+            <li><a href="#portfolio">PORTFOLIO</a></li>
+            <li><a href="#pricing-table">PRICE </a></li>             
+            <li><a href="#from-blog">BLOG </a></li>      
+            <li><a href="#contact">CONTACT</a></li>
+          </ul>                            
+        </div><!--/.nav-collapse -->
+       <div class="search-area">
+          <form action="">
+            <input id="search" name="search" type="text" placeholder="What're you looking for ?">
+            <input id="search_submit" value="Rechercher" type="submit">
+          </form>
+        </div>         
+      </div>          
+    </nav> 
+  </section>
+  <!-- End menu section -->
+
+  <!-- Start about section -->
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <!-- Start welcome area -->
+          <div class="welcome-area">
+            <div class="title-area">
+              <h2 class="tittle">Welcome to <span>Nex</span></h2>
+              <span class="tittle-line"></span>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  labore et dolore magna aliqua. Ut enim ad minim veniamo laboris nis</p>
+            </div>
+            <div class="welcome-content">
+              <ul class="wc-table">
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-users wc-icon"></span>
+                    <h4 class="wc-tittle">Creative concept</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-sellsy wc-icon"></span>
+                    <h4 class="wc-tittle">interface design</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-line-chart wc-icon"></span>
+                    <h4 class="wc-tittle">strategy</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="single-wc-content wow fadeInUp">
+                    <span class="fa fa-bus wc-icon"></span>
+                    <h4 class="wc-tittle">business solution</h4>
+                    <p>At vero eos et accusamus et iusto odio in dignissimos ducimus voluptatum in delenatque </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- End welcome area -->
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="about-area">
+            <div class="row">
+              <div class="col-md-5 col-sm-6 col-xs-12">
+                <div class="about-left wow fadeInLeft">
+                  <img src="<?php echo base_url();?>assets/images/about-img.jpg" alt="img">
+                  <a class="introduction-btn" href="#">Introduction</a>
+                </div>
+              </div>
+              <div class="col-md-7 col-sm-6 col-xs-12">
+                <div class="about-right wow fadeInRight">
+                  <div class="title-area">
+                    <h2 class="tittle">About <span>Nex</span> design</h2>
+                    <span class="tittle-line"></span>
+                    <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illotore itatis et quasi architecto beatae vitae dicta sunt explicabo.</p> 
+
+                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores e</p>
+                    <div class="about-btn-area">
+                      <a href="#" class="button button-default" data-text="KNOW MORE"><span>KNOW MORE</span></a>
+                    </div>                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> 
+  <!-- End about section -->
+
+  <!-- Start call to action -->
+  <section id="call-to-action">
+    <img src="<?php echo base_url();?>assets/images/call-to-action-bg.png" alt="img">
+    <div class="call-to-overlay">
+      <div class="container">
+        <div class="call-to-content wow fadeInUp">
+          <h2>The most impressive templete you will find</h2>
+          <a href="#" class="button button-default" data-text="GET IT NOW"><span>GET IT NOW</span></a>
+        </div>
+      </div>
+    </div> 
+  </section>
+  <!-- End call to action -->
+
+  <!-- Start Team action -->
+  <section id="team">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="team-area">
+            <div class="title-area">
+              <h2 class="tittle">Meet our team</h2>
+              <span class="tittle-line"></span>
+              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto</p>
+            </div>
+            <!-- Start team content -->
+            <div class="team-content">
+              <ul class="team-grid">
+                <li>
+                  <div class="team-item team-img-1 wow fadeInUp">
+                    <div class="team-info">
+                      <p> I must explain to you how all this mistaken idea of denouncing pleasure n</p>
+                      <a href="#"><span class="fa fa-facebook"></span></a>
+                      <a href="#"><span class="fa fa-twitter"></span></a>
+                      <a href="#"><span class="fa fa-pinterest"></span></a>
+                      <a href="#"><span class="fa fa-rss"></span></a>
+                    </div>
+                  </div>
+                  <div class="team-address">
+                    <p>John Richerd</p>
+                    <span>CEO & Founder</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="team-item team-img-2 wow fadeInUp">
+                    <div class="team-info">
+                      <p> I must explain to you how all this mistaken idea of denouncing pleasure n</p>
+                      <a href="#"><span class="fa fa-facebook"></span></a>
+                      <a href="#"><span class="fa fa-twitter"></span></a>
+                      <a href="#"><span class="fa fa-pinterest"></span></a>
+                      <a href="#"><span class="fa fa-rss"></span></a>
+                    </div>
+                  </div>
+                  <div class="team-address">
+                    <p>MIKE BATTON</p>
+                    <span>Co-founder</span>                  
+                  </div>
+                </li>
+                <li>
+                  <div class="team-item team-img-3 wow fadeInUp">
+                    <div class="team-info">
+                      <p> I must explain to you how all this mistaken idea of denouncing pleasure n</p>
+                      <a href="#"><span class="fa fa-facebook"></span></a>
+                      <a href="#"><span class="fa fa-twitter"></span></a>
+                      <a href="#"><span class="fa fa-pinterest"></span></a>
+                      <a href="#"><span class="fa fa-rss"></span></a>
+                    </div>
+                  </div>
+                  <div class="team-address">
+                    <p>JOHN VICK</p>
+                    <span>Developer</span>
+                  </div>
+                </li>
+                  <li>
+                  <div class="team-item team-img-4 wow fadeInUp">
+                    <div class="team-info">
+                      <p> I must explain to you how all this mistaken idea of denouncing pleasure n</p>
+                      <a href="#"><span class="fa fa-facebook"></span></a>
+                      <a href="#"><span class="fa fa-twitter"></span></a>
+                      <a href="#"><span class="fa fa-pinterest"></span></a>
+                      <a href="#"><span class="fa fa-rss"></span></a>
+                    </div>
+                  </div>
+                  <div class="team-address">
+                    <p>ROB HANSOM</p>
+                    <span>Senior designer</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <!-- End team content -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Start Team action -->
